29c749e10f49e2be23c6de8f46f2292c8d707b50
Dogmael <47062956+Dogmael@users.noreply.github.com>
2019-03-29 19:45:25 +0100

    Version avec check bloc non fonctionelle

--- projet.c
+++ projet.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int len = 81;        // taille du tableau modelisant la grille
-int n = 3;          // taille du cote d'un bloc
-int cotegrille = 9; // longueur d'un cote de la grille
-int cotebloc = 3;   // longeur d'un bloc	
+#define TAILLE_MAX 1000
+
+int len = 256;        // taille du tableau modelisant la grille
+int n = 4;          // taille du cote d'un bloc
+int cotegrille = 16; // longueur d'un cote de la grille
+int cotebloc = 4;   // longeur d'un bloc	
 int compteur_tentatives = 0;
 int *pointeur_sur_compteur_tentative = &compteur_tentatives;
 
@@ -21,82 +24,77 @@ int check_colonne(int t[], int numero_colonne); //Elle renvoit 1 si la colonne e
 int check_bloc(int t[], int numero_bloc); //Elle renvoit 1 si le bloc est compatible, O sinon.
 int check_case(int t[], int numero_element);
 
+void print_tableau (int t[], int len) {
+	printf("[");
+	for(int k = 0; k < len - 1; k++) {
+		printf("%2d,", t[k]);
+	}
+	printf("%2d", t[len - 1]);
+	printf("]");
+}
+
 int main() {
 	int t[len];
-	if( read_grid(t) == -1) {
-		printf("La grille ne respect pas le format d'ecriture.\n");
-		return -1;
-	}
 	read_grid(t);
 	print_grid(t);
-	printf("\n\n");
-	solve(t);
-	printf("\n");
-	print_grid(t);	
+	printf("%d\n",check_grid(t));
 	return 0;
 }
 
 int read_grid(int t[]) { 
+	FILE* fichier = NULL;
+    
+    char chaine[TAILLE_MAX] = "";
+ 	int position = 0;
+ 	int i = 0;
+ 	int tailleligne = 4*cotegrille;
+ 	char ligne[tailleligne];
 
- 	FILE* fichier = NULL;
+
     fichier = fopen("test.txt", "r");
+ 	
+ 	if (fichier == NULL) {
+ 		printf("Le fichier est introuvable.\n");
+ 		return -1;
+ 	} else {
+        while (i < cotegrille) {
+        	int j = 0;
+            fgets(ligne, tailleligne, fichier);
+            printf("Ligne lue : '%s'", ligne);
+            char *token;
+            token = strtok(ligne, " ");
 
-	if (fichier == NULL) {
-		printf("Impossible d'ouvrir le fichier test.txt");
-    } else {
-
-    	int deux_dernieres_caracteres[2]; //On mémorise les deux dernieres caratères
-		int caractereActuel = 0;
-		int caractere_converti;
-		int cpt = 0;
-
-		while(caractereActuel != EOF) {
-
-					caractereActuel = fgetc(fichier); // On lit le caractere
-					deux_dernieres_caracteres[0] = deux_dernieres_caracteres[1];
-					deux_dernieres_caracteres[1] = caractereActuel;
-
-					if( (deux_dernieres_caracteres[0] > 48 && deux_dernieres_caracteres[0] < 58) && (deux_dernieres_caracteres[1] > 48 && deux_dernieres_caracteres[1] < 58) ) { //On vérifie qu'il n'y ait pas deux chiffres NON NULS consécutifs.
-						return(-1); //Le programe contient des nombre à plus de deux chiffres.
-					}
-					if( (caractereActuel >= 33 && caractereActuel < 48) || (caractereActuel > 57) ) { //Si on détecte autre chose que des chiffres entre 0 et 9 ou des espaces et retours à la ligne on renvoit un message d'erreur.
-						return(-1); //Un caractère incorect est présent dans le fichier.
-					}
-					if(caractereActuel <= 32 ) {
-						continue; //on ne s'interesse pas à tout ce que n'est pas un chiffre entre 0 et 9
-					} else {
-						caractere_converti = caractereActuel - 48; //On converti les caractères en entiers.
-						t[cpt] = caractere_converti; //On rentrer le caractère dans le tableau.
-						cpt ++;
-					}
-					if(cpt > 81) { //Si on a plus de 81 chiffres on revoit une erreur 
-						return(-1); //Il y a plus de 81 chffre dans le fichier.
-					}
-    	}
-
-    	if(cpt < 81) {
-    		return(-1); //Le fichier ne contient pas asser de chiffre.
-   		 }
-
-   		 for (int k = 0; k < len; k++) { //On verifie si le tableau n'est pas déjà plein.
-  		  	if(t[k] == 0) {return 0;}
-  	 	 }
-    	return(-1); //Le fichier est déjà plein.
-		}
-
-    fclose(fichier);
+            //Afficher le résultat du découpage
+            while (j < cotegrille) {
+                t[position] = atoi(token);
+                position++;
+                token = strtok(NULL, " ");
+                j++;
+            }
+            printf("\n");
+            i++;
+            //printf("valeur de i : %d, valeur de j : %d\n", i,j);
+        }
+        fclose(fichier);
+	}
+	return 0;
 }
+  
 
-int check_bloc(int t[], int element_debut_bloc) {
+int check_bloc(int t[], int element_debut_bloc) { //le tableau bloc géneré n'est pas bon
 	int bloc[cotegrille];
-	int element_fin_bloc = element_debut_bloc + 2*cotegrille + cotebloc -1;
+	int element_fin_bloc = element_debut_bloc + (cotebloc-1)*cotegrille + cotebloc -1; //bien généralisé
 	int curseur = 0; //Il sert à récuperer ou sommes nous dans le tableau bloc[]
 	for(int k = element_debut_bloc; k < element_fin_bloc; k = k + cotegrille) {
 		for (int i = 0; i < cotebloc; i++) {
-			bloc[curseur] = t[k + i];
+			// printf("k = %d, i= %d\n", k,i); //k+i est bon
+			// printf("t[k+i]=%d\n",t[k + i]);
+			printf("curseur = %d\n", curseur);
+			bloc[curseur] = t[k+i];
 			curseur++;
+			printf("bloc[curseur] = %d\n", bloc[curseur]);
 		}
-
+		print_tableau(bloc, cotegrille);
 	}
 	if(check_element(bloc, cotegrille) == 1) {
 		return 0;
@@ -106,12 +104,12 @@ int check_bloc(int t[], int element_debut_bloc) {
 }
 
 int check_case (int t[], int numero_element) {
-	int numero_colonne = numero_element % 9 + 1;
+	int numero_colonne = numero_element % cotegrille + 1;
 	if(check_colonne(t, numero_colonne) == 0) {
 		return 0;
 	}
 
-	int numero_ligne = (numero_element - numero_colonne)/9 +2;
+	int numero_ligne = (numero_element - numero_colonne)/cotegrille +2;
 	if (check_ligne(t,numero_ligne) == 0) {
 		return 0;
 	}
@@ -161,6 +159,7 @@ int check_grid (int t[]) {
 //On verifie que les lignes sont compatibles.
 	for(int numero_ligne = 1; numero_ligne <= cotegrille; numero_ligne++) { //On fait une boucle pour verifier toutes les lignes, elles sont numérotées de 1 à coter grille.
 		if(check_ligne(t, numero_ligne) == 0) {
+			printf("erreur de ligne");
 			return 0;
 		} else {
 			continue;
@@ -170,6 +169,7 @@ int check_grid (int t[]) {
 // On verifie que les colonnes sont compatibles. 
 	for(int numero_colonne = 1; numero_colonne <= cotegrille; numero_colonne++) { //On sait que les éléments de la nieme colonne sont congrus a (n - 1) mod 9
 		if(check_colonne(t, numero_colonne) == 0) {
+			printf("erreur de colonne");
 			return 0;
 		} else {
 			continue;
@@ -180,6 +180,7 @@ int check_grid (int t[]) {
 	for(int element_debut_bloc = 0; element_debut_bloc < len; element_debut_bloc = element_debut_bloc + cotebloc*cotegrille) {
 		for (int curseur_bloc = element_debut_bloc; curseur_bloc < cotebloc; curseur_bloc = curseur_bloc + cotebloc) {
 			if(check_bloc(t, element_debut_bloc) == 0) {
+				printf("erreur de bloc, pour element_debut_bloc = %d", element_debut_bloc);
 				return 0;
 			} else {
 				continue;
@@ -191,8 +192,8 @@ return 1;
 }
 
 int check_element(int tableau[], int longueur_tableau){
-	int masque[9];
-	for(int k = 0; k < 9; k++) {
+	int masque[cotegrille];
+	for(int k = 0; k < cotegrille; k++) {
 		masque[k] = 1;
 	}
 	for(int k = 0; k < longueur_tableau; k++) {
@@ -208,13 +209,13 @@ int check_element(int tableau[], int longueur_tableau){
 }
 
 int solve (int t[]){
-
+	printf("On est dans le solve\n");
 	//On commence par vérifier si la sudoku peut être résolue.
 	if (check_grid(t) == 0){
 		printf("La grille n'est pas compatible.\n");
 		return 1;
 	}
-
+	printf("On est sorie de check_grid du solves\n");
 	//On commence par dubliqué le tableau donné en entré.
 	int t_save[len];
 	for(int k = 0; k <= len; k++){
@@ -232,17 +233,17 @@ int solve (int t[]){
 	//On resout par backtracking
 	while(k < len){
 		//On tente de  trouver une valeur qui fonctionne pour t[k]
-		for(int i = t[k] + 1; i <=9; i++) { //On tente tout les valeurs pour t[k], si aucune n'est valide on remonte et k = previous_modifiable.
+		for(int i = t[k] + 1; i <= cotegrille; i++) { //On tente tout les valeurs pour t[k], si aucune n'est valide on remonte et k = previous_modifiable.
 			t[k] = i; //On associe à notre terme la valeur de i.
 			compteur_tentatives++;
 			if(check_case(t,k) == 1) { 
 				k = next_modifiable(t_save, k); //Si grille reste compatible on passe a la case suivante
-				if(k>80 || k < 0) { //On arrête la boucle while si on est en dehors du tableau.
+				if(k>(len-1) || k < 0) { //On arrête la boucle while si on est en dehors du tableau.
 					return 0;
 				}
 				break;
-			} else if (check_case(t, k) == 0 && i == 9) { //Si on a essayé tout les valeurs pour t[k] jusqu'à 9 et que ça ne fonctionne toujours pas on modifie la case précédente.
-				while (t[k] == 9) { //On va remonter juqu'à trouver une case qui n'est pas déja à 9 et on remet à zero toute celles qu'on rencontre en remontant.
+			} else if (check_case(t, k) == 0 && i == cotegrille) { //Si on a essayé tout les valeurs pour t[k] jusqu'à 9 et que ça ne fonctionne toujours pas on modifie la case précédente.
+				while (t[k] == cotegrille) { //On va remonter juqu'à trouver une case qui n'est pas déja à 9 et on remet à zero toute celles qu'on rencontre en remontant.
 					t[k] = 0; //On remet k à 0 avant de remonter dans le tableau
 					k = previous_modifiable(t_save, k);
 				}
